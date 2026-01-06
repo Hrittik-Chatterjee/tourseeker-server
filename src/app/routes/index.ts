7198249d4ac9f13b617ea27ad3cbd7dc8b3dbd6f
hrittik-chatterjee <hrittik0chatterjee@gmail.com>
2026-01-07 03:30:02 +0600

    added global route in index.ts

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
+import { AuthRoutes } from "../modules/auth/auth.routes";
+import { UserRoutes } from "../modules/user/user.routes";
 
 const router = express.Router();
 
 const moduleRoutes = [
-  // Add module routes here as we create them
-  // Example:
-  // {
-  //   path: "/auth",
-  //   route: authRoutes,
-  // },
+  {
+    path: "/auth",
+    route: AuthRoutes,
+  },
+  {
+    path: "/users",
+    route: UserRoutes,
+  },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
